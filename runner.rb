be1ee073f3b9c20f20de30c3a07d6f7e5800a14b
Style Mistake <stylemistake@gmail.com>
2017-07-04 21:20:16 +0300

    Bump to 0.8.0

--- runner.rb
+++ runner.rb
@@ -1,8 +1,8 @@
 class Runner < Formula
   desc "Task runner for bash"
   homepage "https://github.com/stylemistake/runner"
-  url "https://github.com/stylemistake/runner/archive/v0.7.1.tar.gz"
-  sha256 "06885310234788eef7416615cc92681f09e1a8d6f97ece08e9a863fb57e60039"
+  url "https://github.com/stylemistake/runner/archive/v0.8.0.tar.gz"
+  sha256 "0b8e0dd41c42a278fc6b78a83ff4f2933317e153bf53ef2d4c07fb36de4bf8fe"
 
   def install
     prefix.install "src"
